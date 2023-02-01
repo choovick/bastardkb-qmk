921d690d23e5b3862d0de48c491244e7123819e8
Alexey Popovich <alexey.popovich@bluescape.com>
2023-02-01 09:01:57 -0500

    Default map tweaks

--- keyboards/bastardkb/charybdis/4x6/keymaps/via/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/via/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_RCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LSFT,  PT_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, PT_SLSH, KC_RSFT,
+       KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_Z, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                    MO(1), KC_LGUI, KC_LALT,     LT(3, KC_BSPC), KC_SPACE,
                                          KC_RCTL, MO(2),       LT(2, KC_ENTER)
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        KC_ESCAPE, KC_F1,   KC_F2, KC_F3,  KC_F4, KC_F5,                KC_F6,    KC_F7,   KC_F8,  KC_F9, KC_F10,  KC_TRNS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_TRNS, KC_LGUI, KC_LALT, SNIPING, KC_BTN4, KC_BTN5,          KC_PGUP,  KC_HOME, KC_UP,  KC_P9, KC_F11, KC_F12,
+       LALT(KC_TAB), KC_LGUI, KC_LALT, SNIPING, KC_BTN4, KC_BTN5,          KC_PGUP,  KC_HOME, KC_UP,  KC_P9, KC_F11, KC_F12,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
 KC_RCTL, LGUI(KC_A), LSFT(KC_BTN3), KC_BTN2, KC_BTN1, KC_BTN3,        KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_MINUS, KC_EQUAL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
